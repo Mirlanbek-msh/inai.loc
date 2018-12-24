670176b21ef301e7c605b5e06392941be18285e2
Erkebek <erkebek.ab@gmail.com>
2018-12-24 22:42:36 +0600

    added composer script

--- app/overrides/mcamara/laravel-localization/src/Mcamara/LaravelLocalization/LaravelLocalization.php
+++ app/overrides/mcamara/laravel-localization/src/Mcamara/LaravelLocalization/LaravelLocalization.php
@@ -889,11 +889,11 @@ class LaravelLocalization
                 }
             }
         } else {
-            if (!$this->router->current()) {
+            if (!$app['router']->current()) {
                 return [];
             }
 
-            $attributes = $this->normalizeAttributes($this->router->current()->parameters());
+            $attributes = $this->normalizeAttributes($this->app['router']->current()->parameters());
             $response = event('routes.translation', [$locale, $attributes]);
 
             if (!empty($response)) {
