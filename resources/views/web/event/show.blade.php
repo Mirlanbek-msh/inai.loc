2a8468c4c0ad6c5472c47c5d4f561f70eff9584a
Erkebek <erkebek.ab@gmail.com>
2019-01-08 23:25:07 +0600

    event image og fixed

--- resources/views/web/event/show.blade.php
+++ resources/views/web/event/show.blade.php
@@ -16,7 +16,7 @@
     <meta property="og:type" content="article">
     <meta property="og:site_name" content="inai.kg">
     <meta property="og:image:type" content="image/jpg">
-    <meta property="og:image:width" content="1280">
+    <meta property="og:image:width" content="720">
     <meta property="og:image:height" content="720">
     <meta property="og:url" content="{{ route('web.post.show', $row->slug) }}">
     <meta property="og:image" content="{{ asset($row->image) }}">
