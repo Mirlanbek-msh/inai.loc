57efe58bbe03fe5aa452bd60f21b5da131b00930
Erkebek <erkebek.ab@gmail.com>
2019-01-10 01:29:51 +0600

    changed og image

--- resources/views/partials/ogdata.blade.php
+++ resources/views/partials/ogdata.blade.php
@@ -1,6 +1,5 @@
-<meta property="og:image:height" content="1257">
-<meta property="og:image:width" content="2400">
-<meta property="og:url" content="http://inai.kg">
 <meta property="og:image" content="{{ asset('/ogassets/og-image.jpg') }}">
-<meta property="og:locale" content="{{ str_replace('_', '-', app()->getLocale()) }}" />
+<meta property="og:image:width" content="279">
+<meta property="og:image:height" content="279">
+<meta property="og:url" content="http://inai.kg">
 <meta property="og:site_name" content="inai.kg">
