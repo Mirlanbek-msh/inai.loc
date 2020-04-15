40adf678575ef15219386354023c57115fd87e36
Erkebek Abdrakhman uulu <erkebek.ab@gmail.com>
2020-04-15 18:52:10 +0200

    embedded items improved

--- resources/views/web/partials/_embedded.blade.php
+++ resources/views/web/partials/_embedded.blade.php
@@ -2,5 +2,7 @@
     if($('iframe[allowfullscreen]').length){
         $('iframe[allowfullscreen]').parent().addClass('embed-responsive embed-responsive-16by9');
         $('iframe[allowfullscreen]').addClass('embed-responsive-item');
+        var src = $('iframe[allowfullscreen]').attr('src');
+        $('iframe[allowfullscreen]').attr('src', src + '?rel=0&amp;showinfo=0');
     }
 </script>
